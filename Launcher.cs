b4c26d192367e9489e2bec21387eb2ebb8b8c37a
I-Lex <i.lex.iustitiae@gmail.com>
2020-05-29 01:28:35 +0200

    Initial push

---
+++ Launcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WhoIsResponsible
+{
+    class Launcher
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ProjectSelection());
+        }
+
+    }
+}
---
+++ ProjectSelection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WhoIsResponsible
+{
+    public partial class ProjectSelection : Form
+    {
+        public ProjectSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenProjectButton_Click(object sender, EventArgs e)
+        {
+            if (openProjectDialog.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openProjectDialog.FileName;
+            }
+
+        }
+    }
+}
